36a1c80cd4083e08cbebf38af4be83cbc0f28b12
itsdeebarnett <jdeebarnett@gmail.com>
2021-07-19 02:49:39 -0400

    serializers and url

--- drf_adventure/urls.py
+++ drf_adventure/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include,path
+from rest_framework.urlpatterns import format_suffix_patterns
+from books import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('books/', views.BookList.as_view(), name='book-list'),
+    path('books/<int:pk>/', views.BookDetail.as_view(), name='book-detail'),
+
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
